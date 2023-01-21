65b93864ea18195ba09f4ed9571c83ab1b9cd3d5
Janek1010 <janekdomozych99@gmail.com>
2023-01-21 11:58:54 +0100

    CricketCoach -> Ne fields with setters and initialization in context - emailAddress, team

--- SpringRow/src/com/luv2code/springdemo/CricketCoach.java
+++ SpringRow/src/com/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,10 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach{
     private FortuneService fortuneService;
 
+    private String emailAddress;
+    private String team;
+
+
     // create a no argument constructor
 
     public CricketCoach() {
@@ -11,8 +15,30 @@ public class CricketCoach implements Coach{
 
     // setter
     public void setFortuneService(FortuneService fortuneService) {
-        System.out.println("CricketCoach: inside setter");
+        System.out.println("CricketCoach: inside setter setFortuneService");
         this.fortuneService = fortuneService;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter setTeam");
+        this.team = team;
+    }
+
+    public FortuneService getFortuneService() {
+        return fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
     @Override
